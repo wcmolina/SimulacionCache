509886daec07104fd7dd2e01eb03e8d80d8dcec3
Wilmer Carranza <wcarranzam91@gmail.com>
2017-06-15 09:08:55 -0600

    Se quitaron imports extra

--- src/Simulacion.java
+++ src/Simulacion.java
@@ -1,9 +1,9 @@
-import java.io.*;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Locale;
 
 public class Simulacion {
     //RAM = 4096 bytes
